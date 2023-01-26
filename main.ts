6a14c448af5f0680840ae6acc0b276cc17e30611
Emirocksr <122539529+Emirocksr@users.noreply.github.com>
2023-01-26 08:44:10 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    emir += 1
+    basic.showNumber(emir)
+})
+input.onButtonPressed(Button.AB, function () {
+    emir = 0
+    basic.showNumber(emir)
+})
+input.onButtonPressed(Button.B, function () {
+    emir += -1
+    basic.showNumber(emir)
+})
+let emir = 0
+emir = 0
+basic.showNumber(emir)
+basic.forever(function () {
+	
+})
